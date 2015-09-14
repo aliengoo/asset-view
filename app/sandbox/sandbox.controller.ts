6eb00c0558221dc233f0a8d9a62256d51cea0baa
Alien Goo <glenn.android@gmail.com>
2015-09-14 23:09:40 +0100

    wip

--- app/sandbox/sandbox.controller.ts
+++ app/sandbox/sandbox.controller.ts
@@ -4,9 +4,24 @@ module av {
 
   export class SandboxController {
 
+    public sandbox:HTMLCanvasElement;
+    public ctx:CanvasRenderingContext2D;
+
     /* @ngInject */
     constructor() {
 
+      this.sandbox = <HTMLCanvasElement>document.getElementById("sandbox");
+
+      this.ctx = this.sandbox.getContext("2d");
+
+      this.ctx.fillStyle = "rgb(200,0,0)";
+      this.ctx.fillRect (10, 10, 55, 50);
+
+      this.ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+      this.ctx.fillRect (30, 30, 55, 50);
+
+      this.ctx.font = 'italic 40pt Calibri';
+      this.ctx.fillText('Hello World!', 150, 100);
     }
   }
 }
